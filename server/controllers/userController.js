312b2e79e9aba2b8636b59ba1803b4e9430bfe29
Павел <107314499+Key4000@users.noreply.github.com>
2025-09-19 13:04:18 +0500

    Add name on registration

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -30,22 +30,29 @@ class UserController {
 
     //регистрация 
     async registration(req, res, next) {
-        const { email, password } = req.body
+        const { email, password, name} = req.body
+//- - - - - - валидация - - - - -
         //если пароля и емейла нету
         if (!email || !password) {
             return next(ApiError.badRequest('Некорректный email или password'))
         }
         //проверить если такой емейл есть
         const candidate = await User.findOne({ where: { email } })
-
         if (candidate) {
             return next(ApiError.badRequest('Пользователь с таким email уже существует'))
         }
+        //проверить если такое имя есть
+        const candidate = await User.findOne({ where: { name } })
+        if (candidate) {
+            return next(ApiError.badRequest('Пользователь с таким именем уже существует'))
+      } 
+//- - - - - - - - - 
+
         //хешируем пароль (пароль, сколько раз хешируем - 5 раз)
         const hashPassword = await bcrypt.hash(password, 5)
 
         //создаем пользователя
-        const user = await User.create({ email, password: hashPassword })
+        const user = await User.create({ email, password: hashPassword, name})
         
         
         //генерируем токен
@@ -57,12 +64,12 @@ class UserController {
     //авторизация
     async login(req, res, next) {
         //вытаскиваем емаил и пасс из тела запроса  
-        const { email, password } = req.body
+        const { email, password} = req.body
         //ищем юзера по емэйлу 
         const user = await User.findOne({ where: { email } })
-
+// - - - - - - - валидация - - - - - 
         if (!user) {
-            next(ApiError.internal('пользователь с таким email не найден'))
+           next(ApiError.internal('пользователь с таким email не найден'))
         }
         //сравниваем пароли(1 аргумент, пароль что написал пользователь, 2 пароль из базы данных)
         let comparePassword = bcrypt.compareSync(password, user.password)
@@ -70,6 +77,7 @@ class UserController {
         if (!comparePassword) {
             next(ApiError.internal('Пароль не верный'))
         }
+// - - - - - - - - 
 
         //генерируем jwt токен userid basketid
         const token = generateJwt(user.id, user.email)
