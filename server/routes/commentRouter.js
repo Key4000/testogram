9a6701fd767108e96fc5938bcc119c35c4e9e863
Павел <107314499+Key4000@users.noreply.github.com>
2025-09-30 15:28:05 +0500

    Update commentRouter.js

--- server/routes/commentRouter.js
+++ server/routes/commentRouter.js
@@ -15,13 +15,12 @@ const commentController =  require('../controllers/commentController')
 //------------Маршруты-----------------
 
 //получение всех комментов публикации
-router.get('/', commentController.getAll)
+router.get('/:publicationId', commentController.getAll)
 
 //добавление коммента публикации 
 router.post('/', commentController.add)
 
-//получение одного коммента публикации 
-router.get('/:id', commentController.getOne)
+
 
 
 module.exports = router
