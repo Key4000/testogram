0ec2853b544ff39820d99c7c57fc1eb0bc6c744f
Павел <107314499+Key4000@users.noreply.github.com>
2025-10-28 12:36:48 +0500

    Update PostStore.js

--- client/src/store/PostStore.js
+++ client/src/store/PostStore.js
@@ -10,6 +10,8 @@ export default class PostStore {
 
     //посты
     this._posts = []
+    //кол-во
+    this._count = 0
     
     //
     makeAutoObservable(this)
@@ -19,9 +21,16 @@ export default class PostStore {
   setPosts(posts) {
     this._posts = posts
   }
+  setCount(num) {
+    this._count = num
+  }
   //гетеры
   get posts() {
     return this._posts
   }
+  get count() {
+    return this._count
+  }
+  
   
 }
