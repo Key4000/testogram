d743444d38b6f4db361c9f739c4c0f340fe15e51
Павел <107314499+Key4000@users.noreply.github.com>
2025-11-13 11:22:50 +0500

    Fixed link in subscription window

--- client/src/components/Modals/SubscriptionWindow.js
+++ client/src/components/Modals/SubscriptionWindow.js
@@ -6,9 +6,7 @@
 import React, { useEffect } from 'react'
 import { Container, Modal, ListGroup } from 'react-bootstrap';
 import { Context } from '../../index';
-import { useContext, useState } from 'react';
-import { useNavigate } from 'react-router-dom'
-import { PROFILE_ROUTE } from '../../utils/consts'
+import { useContext } from 'react';
 import { observer } from 'mobx-react-lite'
 import Logo from '../Logo/Logo'
 import { getAvatar, getName } from "../../http/userAPI"
@@ -19,15 +17,11 @@ const SubscriptionWindow = observer(({ show, onHide }) => {
   //Получаем хранилище
   const { sub } = useContext(Context)
 
-  const navigate = useNavigate();
-
-  let subArr = []
-
    useEffect(() => {
      //подгружаем имена и аватарки к подпискам
      sub.subscription.map(person => {
-       getAvatar(person.subId).then(data => { person.avatar = data })
-       getName(person.subId).then(data => { person.name = data })
+       getAvatar(person.subId).then(data => { person.avatar = data.img})
+       getName(person.subId).then(data => { person.name = data.name })
      })
    }, [])
 
@@ -42,14 +36,13 @@ const SubscriptionWindow = observer(({ show, onHide }) => {
       <Modal.Body>
          {show && <ListGroup>
           {sub.subscription.map(person =>
-            <ListGroup.Item
-              style={{ cursor: "pointer" }}
-              key={person.id}
-              // onClick={navigate(PROFILE_ROUTE + '/' + person.subId)}
-            >
-              <Logo src={person.avatar}></Logo>
-              {person.name}
-            </ListGroup.Item>
+<ListGroup.Item
+  style={{ cursor: "pointer" }}
+  key={person.id}
+>
+  <Logo src={person.avatar}></Logo>
+  <Link to={PROFILE_ROUTE + '/' + person.subId} reloadDocument style={{ marginLeft: "20px", cursor: "pointer", color: "black", textDecoration: "none" }}>{person.name}</Link>
+</ListGroup.Item>
           )}
         </ListGroup>}
       </Modal.Body>
